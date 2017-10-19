Phyks/Flatisfy

commit 88a923c87ee86151cbc316a75b32feadc854231e

Fix for https://github.com/Phyks/Flatisfy/issues/5

flatisfy/config.py
@@ -16,7 +16,9 @@ import traceback
 
 import appdirs
 
+from flatisfy import data
 from flatisfy import tools
+from flatisfy.models.postal_code import PostalCode
 
 
 # Default configuration
@@ -106,6 +108,26 @@ def validate_config(config):
         # message in the log output.
         # pylint: disable=locally-disabled,line-too-long
 
+        assert config["passes"] in [0, 1, 2, 3]
+        assert config["max_entries"] is None or (isinstance(config["max_entries"], int) and config["max_entries"] > 0)  # noqa: E501
+
+        assert config["data_directory"] is None or isinstance(config["data_directory"], str)  # noqa: E501
+        assert os.path.isdir(config["data_directory"])
+        assert isinstance(config["search_index"], str)
+        assert config["modules_path"] is None or isinstance(config["modules_path"], str)  # noqa: E501
+
+        assert config["database"] is None or isinstance(config["database"], str)  # noqa: E501
+
+        assert isinstance(config["port"], int)
+        assert isinstance(config["host"], str)
+        assert config["webserver"] is None or isinstance(config["webserver"], str)  # noqa: E501
+        assert config["backends"] is None or isinstance(config["backends"], list)  # noqa: E501
+
+        assert isinstance(config["send_email"], bool)
+        assert config["smtp_server"] is None or isinstance(config["smtp_server"], (str, unicode))  # noqa: E501
+        assert config["smtp_port"] is None or isinstance(config["smtp_port"], int)  # noqa: E501
+        assert config["smtp_to"] is None or isinstance(config["smtp_to"], list)
+
         # Ensure constraints are ok
         assert config["constraints"]
         for constraint in config["constraints"].values():
@@ -120,6 +142,12 @@ def validate_config(config):
 
             assert "postal_codes" in constraint
             assert constraint["postal_codes"]
+            opendata_postal_codes = [
+                x.postal_code
+                for x in data.load_data(PostalCode, constraint, config)
+            ]
+            for postal_code in constraint["postal_codes"]:
+                assert postal_code in opendata_postal_codes  # noqa: E501
 
             assert "area" in constraint
             _check_constraints_bounds(constraint["area"])
@@ -142,26 +170,6 @@ def validate_config(config):
                 assert len(item["gps"]) == 2
                 assert "time" in item
                 _check_constraints_bounds(item["time"])
-
-        assert config["passes"] in [0, 1, 2, 3]
-        assert config["max_entries"] is None or (isinstance(config["max_entries"], int) and config["max_entries"] > 0)  # noqa: E501
-
-        assert config["data_directory"] is None or isinstance(config["data_directory"], str)  # noqa: E501
-        assert os.path.isdir(config["data_directory"])
-        assert isinstance(config["search_index"], str)
-        assert config["modules_path"] is None or isinstance(config["modules_path"], str)  # noqa: E501
-
-        assert config["database"] is None or isinstance(config["database"], str)  # noqa: E501
-
-        assert isinstance(config["port"], int)
-        assert isinstance(config["host"], str)
-        assert config["webserver"] is None or isinstance(config["webserver"], str)  # noqa: E501
-        assert config["backends"] is None or isinstance(config["backends"], list)  # noqa: E501
-
-        assert isinstance(config["send_email"], bool)
-        assert config["smtp_server"] is None or isinstance(config["smtp_server"], (str, unicode))
-        assert config["smtp_port"] is None or isinstance(config["smtp_port"], int)
-        assert config["smtp_to"] is None or isinstance(config["smtp_to"], list)
 
         return True
     except (AssertionError, KeyError):

commit f1a5535bca44559a18657c7c3bf3ab7f99f14023

Fix an error introduced in 88a923c87ee86151cbc316a75b32feadc854231e

flatisfy/__main__.py
@@ -142,7 +142,11 @@ def main():
         sys.exit(0)
     else:
         # Load config
-        config = flatisfy.config.load_config(args)
+        if args.cmd == "build-data":
+            # Data not yet built, do not use it in config checks
+            config = flatisfy.config.load_config(args, check_with_data=False)
+        else:
+            config = flatisfy.config.load_config(args, check_with_data=True)
         if config is None:
             LOGGER.error("Invalid configuration. Exiting. "
                          "Run init-config before if this is the first time "

flatisfy/config.py
@@ -78,11 +78,13 @@ DEFAULT_CONFIG = {
 LOGGER = logging.getLogger(__name__)
 
 
-def validate_config(config):
+def validate_config(config, check_with_data):
     """
     Check that the config passed as argument is a valid configuration.
 
     :param config: A config dictionary to fetch.
+    :param check_with_data: Whether we should use the available OpenData to
+    check the config values.
     :return: ``True`` if the configuration is valid, ``False`` otherwise.
     """
     def _check_constraints_bounds(bounds):
@@ -142,12 +144,13 @@ def validate_config(config):
 
             assert "postal_codes" in constraint
             assert constraint["postal_codes"]
-            opendata_postal_codes = [
-                x.postal_code
-                for x in data.load_data(PostalCode, constraint, config)
-            ]
-            for postal_code in constraint["postal_codes"]:
-                assert postal_code in opendata_postal_codes  # noqa: E501
+            if check_with_data:
+                opendata_postal_codes = [
+                    x.postal_code
+                    for x in data.load_data(PostalCode, constraint, config)
+                ]
+                for postal_code in constraint["postal_codes"]:
+                    assert postal_code in opendata_postal_codes  # noqa: E501
 
             assert "area" in constraint
             _check_constraints_bounds(constraint["area"])
@@ -177,11 +180,13 @@ def validate_config(config):
         return traceback.extract_tb(exc_traceback)[-1][-1]
 
 
-def load_config(args=None):
+def load_config(args=None, check_with_data=True):
     """
     Load the configuration from file.
 
     :param args: An argparse args structure.
+    :param check_with_data: Whether we should use the available OpenData to
+    check the config values. Defaults to ``True``.
     :return: The loaded config dict.
     """
     LOGGER.info("Initializing configuration...")
@@ -269,7 +274,7 @@ def load_config(args=None):
         if config_data["website_url"][-1] != '/':
             config_data["website_url"] += '/'
 
-    config_validation = validate_config(config_data)
+    config_validation = validate_config(config_data, check_with_data)
     if config_validation is True:
         LOGGER.info("Config has been fully initialized.")
         return config_data