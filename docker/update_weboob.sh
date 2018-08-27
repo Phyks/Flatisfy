bd00a5c07690b1f9a4187fed3249192863c45e44
Benjamin Bouvier <public@benj.me>
2018-08-27 12:11:48 +0200

    Docker: ignore errors in the update_weboob.sh script;

--- docker/update_weboob.sh
+++ docker/update_weboob.sh
@@ -3,5 +3,4 @@ set -e
 
 echo "Updating Weboob..."
 cd /home/user/weboob
-git pull
-pip install --upgrade .
+git pull && pip install --upgrade . || echo "Couldn't upgrade Weboob; maybe the server is unreachable?"
