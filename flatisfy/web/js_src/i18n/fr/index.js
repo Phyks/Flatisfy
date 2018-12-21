ff9c46dac5e47be343fff09bec0617806bffc82b
dohseven <tho_jea+framagit@protonmail.com>
2018-12-21 17:09:12 +0100

    Add French translations

---
+++ flatisfy/web/js_src/i18n/fr/index.js
@@ -0,0 +1,71 @@
+export default {
+    common: {
+        'flats': 'appartement | appartements',
+        'loading': 'Chargement…',
+        'Actions': 'Actions',
+        'More_about': 'Plus sur',
+        'Remove': 'Enlever',
+        'Restore': 'Remettre',
+        'Original_post': 'Annonce originale | Annonces originales',
+        'Original_post_for': 'Annonce originale pour',
+        'Follow': 'Suivre',
+        'Unfollow': 'Arrêter de suivre',
+        'Close': 'Fermer',
+        'sortUp': 'Trier par ordre croissant',
+        'sortDown': 'Trier par ordre décroissant',
+        'mins': 'min | mins',
+        'Unknown': 'Inconnu',
+        'expired': 'expiré'
+    },
+    home: {
+        'new_available_flats': 'Nouveaux appartements disponibles',
+        'show_expired_flats': 'Montrer les annonces expirées'
+    },
+    flatListing: {
+        'no_available_flats': 'Pas d\'appartement disponible.',
+        'no_matching_flats': 'Pas d\'appartement correspondant.'
+    },
+    menu: {
+        'available_flats': 'Appartements disponibles',
+        'followed_flats': 'Appartements suivis',
+        'by_status': 'Appartements par statut',
+        'search': 'Rechercher'
+    },
+    flatsDetails: {
+        'Notation': 'Note',
+        'Title': 'Titre',
+        'Area': 'Surface',
+        'Rooms': 'Pièces',
+        'Cost': 'Coût',
+        'SquareMeterCost': 'Coût / m²',
+        'utilities_included': '(charges comprises)',
+        'utilities_excluded': '(charges non comprises)',
+        'Description': 'Description',
+        'Details': 'Détails',
+        'Metadata': 'Metadonnées',
+        'postal_code': 'Code postal',
+        'nearby_stations': 'Stations proches',
+        'Times_to': 'Temps jusqu\'à',
+        'Location': 'Localisation',
+        'Contact': 'Contact',
+        'Visit': 'Visite',
+        'setDateOfVisit': 'Entrer une date de visite',
+        'no_phone_found': 'Pas de numéro de téléphone trouvé',
+        'rooms': 'pièce | pièces',
+        'bedrooms': 'chambre | chambres'
+    },
+    status: {
+        'new': 'nouveau',
+        'followed': 'suivi',
+        'ignored': 'ignoré',
+        'user_deleted': 'effacé',
+        'duplicate': 'en double'
+    },
+    slider: {
+        'Fullscreen_photo': 'Photo en plein écran'
+    },
+    search: {
+        'input_placeholder': 'Tapez n\'importe quoi à rechercher…',
+        'Search': 'Chercher !'
+    }
+}
