ebd031c047f40abd0989643c341dcfb7ee2e7c30
Phyks (Lucas Verney) <phyks@phyks.me>
2021-04-28 19:54:18 +0200

    Revert "Fix bug"
    This reverts commit cbe9461809fec8eb78ec79249161e34f32db50e6.

--- flatisfy/fetch.py
+++ flatisfy/fetch.py
@@ -308,7 +308,7 @@ def fetch_flats(config):
                 last_fetch_in_db.last_fetch = arrow.get(
                     last_fetch_by_backends[backend]
                 ).date()
-            elif last_fetch_by_backends[backend]:
+            else:
                 last_fetch_in_db = last_fetch_model.LastFetch(
                     constraint_name=constraint_name,
                     backend=backend,
